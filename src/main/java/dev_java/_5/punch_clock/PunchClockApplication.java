73555228f43f52602b3e6e7bf403e94ce15f10b6
Dutkercz <dutkercz@gmail.com>
2025-03-10 18:14:35 -0300

    First commit

---
+++ src/main/java/dev_java/_5/punch_clock/PunchClockApplication.java
@@ -0,0 +1,13 @@
+package dev_java._5.punch_clock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PunchClockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PunchClockApplication.class, args);
+	}
+
+}
